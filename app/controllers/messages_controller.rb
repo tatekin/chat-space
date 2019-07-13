3ec5778f0bec375ff3f4e23635866497445a8c81
manbow-42 <yuukisasaki42@icloud.com>
2019-07-13 16:55:29 +0900

    Summary

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,8 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+  
   def index
     @message = Message.new
-
     @messages = @group.messages.includes(:user)
   end
 
--- app/models/group.rb
+++ app/models/group.rb
@@ -6,10 +6,10 @@ class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def show_last_message
-    if (last_message = message.last).present?
-      last_message.content? ? last_message.content : "画像が投稿されています"
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
     else
-      "まだメッセージはありません。"
+      'まだメッセージはありません。'
     end
   end
 end
